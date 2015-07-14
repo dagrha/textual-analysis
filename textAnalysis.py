54620f0c030bae8cb0db345c3b5d1780bb5e959b
Nathan Greeney <ngreeney@gmail.com>
2015-07-14 14:48:57 -0600

    copied notebook code to .py file
    made run-able file and testing pull request on GitHub.

---
+++ textAnalysis.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 14:29:58 2015
+
+@author: dagrha & ngreeney
+"""
+
+from textblob import TextBlob
+import pandas as pd
+import pylab as plt
+import fileinput
+import sys
+# %matplotlib inline
+
+with open (r'lovecraft.txt', 'r') as myfile:
+    shunned = myfile.read()
+
+ushunned = unicode(shunned, 'utf-8')
+
+tb = TextBlob(ushunned)
+
+paragraph = tb.sentences[:-120:]
+
+i = -1
+for sentence in paragraph:
+    i += 1
+    sentiment = sentence.sentiment
+    if i == 0:
+        write_type = 'w'
+        with open('shunned.csv', write_type) as text_file:
+            header = 'number,' + 'polarity,' + 'subjectivity\n'
+            text_file.write(str(header))
+    write_type = 'a'
+    with open('shunned.csv', write_type) as text_file:
+        newline = str(i) + ',' + str(sentiment) + '\n'
+        text_file.write(str(newline))
+
+def replace_all(file, search_exp, replace_exp):
+    for line in fileinput.input(file, inplace=1):
+        if search_exp in line:
+            line = line.replace(search_exp,replace_exp)
+        sys.stdout.write(line)
+
+replace_all('shunned.csv', 'Sentiment(polarity=', '')
+replace_all('shunned.csv', ' subjectivity=', '')
+replace_all('shunned.csv', ')', '')
+
+df = pd.DataFrame.from_csv('shunned.csv')
+
+df.polarity.plot(figsize=(12,5), color='b', title='Sentiment Polarity for HP Lovecraft\'s The Shunned House')
+plt.xlabel('Sentence number')
+plt.ylabel('Sentiment polarity')
+
+df['cum_sum'] = df.polarity.cumsum()
+
+df.cum_sum.plot(figsize=(12,5), color='r', 
+                title='Sentiment Polarity cumulative summation for HP Lovecraft\'s The Shunned House')
+plt.xlabel('Sentence number')
+plt.ylabel('Cumulative sum of sentiment polarity')
+
+df.head()
+
+df.describe()
+
+
+for i in df[df.polarity < -0.5].index:
+    print i, tb.sentences[i]
+
