76f4b7a464cdd1fc931e0293be71ad2a5b24ce66
dagrha <dghallau@gmail.com>
2015-07-20 22:33:40 -0600

    simplified code

--- textAnalysis.py
+++ textAnalysis.py
@@ -8,8 +8,8 @@ Created on Tue Jul 14 14:29:58 2015
 from textblob import TextBlob
 import pandas as pd
 import pylab as plt
-import fileinput
-import sys
+import collections
+import re
 
 with open (r'lovecraft.txt', 'r') as myfile:
     shunned = myfile.read()
@@ -23,32 +23,22 @@ paragraph = tb.sentences[:-120:]
 i = -1
 for sentence in paragraph:
     i += 1
-    sentiment = sentence.sentiment
+    pol = sentence.sentiment.polarity
     if i == 0:
         write_type = 'w'
         with open('shunned.csv', write_type) as text_file:
-            header = 'number,' + 'polarity,' + 'subjectivity\n'
+            header = 'number,' + 'polarity,' + '\n'
             text_file.write(str(header))
     write_type = 'a'
     with open('shunned.csv', write_type) as text_file:
-        newline = str(i) + ',' + str(sentiment) + '\n'
+        newline = str(i) + ',' + str(pol) + '\n'
         text_file.write(str(newline))
-
-def replace_all(file, search_exp, replace_exp):
-    for line in fileinput.input(file, inplace=1):
-        if search_exp in line:
-            line = line.replace(search_exp,replace_exp)
-        sys.stdout.write(line)
-
-replace_all('shunned.csv', 'Sentiment(polarity=', '')
-replace_all('shunned.csv', ' subjectivity=', '')
-replace_all('shunned.csv', ')', '')
 
 df = pd.DataFrame.from_csv('shunned.csv')
 
-bookTitle = 'HP Lovecraft\'s The Shunned House'
+book_title = 'HP Lovecraft\'s The Shunned House'
 plt.figure()
-df.polarity.plot(figsize=(12,5), color='b', title='Sentiment Polarity for\n'+bookTitle)
+df.polarity.plot(figsize=(12,5), color='b', title='Sentiment Polarity for\n'+book_title)
 plt.xlabel('Sentence number')
 plt.ylabel('Sentiment polarity')
 
@@ -56,15 +46,18 @@ df['cum_sum'] = df.polarity.cumsum()
 
 plt.figure()
 df.cum_sum.plot(figsize=(12,5), color='r', 
-                title='Sentiment Polarity cumulative summation for\n'+bookTitle)
+                title='Sentiment Polarity cumulative summation for\n'+book_title)
 plt.xlabel('Sentence number')
 plt.ylabel('Sum of Sentiment')
 
 df.head()
-
 df.describe()
-
 
 for i in df[df.polarity < -0.5].index:
     print i, tb.sentences[i]
 
+words = re.findall(r'\w+', open('lovecraft.txt').read().lower())
+common = collections.Counter(words).most_common(10)
+
+df_freq = pd.DataFrame(common, columns=['word', 'freq'])
+df_freq.set_index('word').head()
